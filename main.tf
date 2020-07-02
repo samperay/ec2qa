b92e6bb335392c40920102b0310763821f554d09
Sunil Amperayani <sunil.amperayani@BRILLIO.COM>
2020-07-02 13:02:41 +0530

    removed local-remote exec provisioners

--- main.tf
+++ main.tf
@@ -17,26 +17,6 @@ locals {
 resource "aws_instance" "webserver" {
   ami           = "ami-0447a12f28fddb066"
   instance_type = "t2.micro"
-  key_name      = "ebsdemo"
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo amazon-linux-extras install -y nginx1.12",
-      "sudo systemctl start nginx"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("./ebsdemo.pem")
-      host        = self.public_ip
-    }
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.webserver.public_ip} >> public_ip.txt"
-  }
-
   tags = local.common_tags
 }
 
